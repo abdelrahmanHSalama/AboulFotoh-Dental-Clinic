507ad221c30bc4ba6e942fa48a2566a61a109a31
Abdelrahman <3bdlrhmnHSalama@gmail.com>
2025-03-11 14:05:30 +0200

    Implement WhatsApp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
 const appointmentButton = document.querySelector("#appointment-button");
 
 appointmentButton.addEventListener("click", (e) => {
-    console.log(e);
+    e.preventDefault();
+
+    const name = document.querySelector("#name").value;
+    const message = document.querySelector("#message").value;
+    const waMessage = `الاسم: ${name}%0Aالشكوى: ${message}`;
+
+    window.open(`https://wa.me/201002414958?text=${waMessage}`, "_blank");
 });
